b0143c2eaebec37f762637953481456b2357fbf6
2peaks <woosang@hotmail.com>
2022-01-25 17:00:42 -0800

    Change to use MySql for authentication instead of in memory.

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,15 +1,15 @@
 package com.eazybytes.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -51,15 +51,29 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 // ################################################################
 	
 // ################################################################
-//	In memory authentication another way. You have to pass a PasswordEncoder as a @Bean
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-		UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
-		UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
-		userDetailsService.createUser(user);
-		userDetailsService.createUser(user1);
-		auth.userDetailsService(userDetailsService);
+////	In memory authentication another way. You have to pass a PasswordEncoder as a @Bean
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+//		UserDetails user = User.withUsername("admin").password("12345").authorities("admin").build();
+//		UserDetails user1 = User.withUsername("user").password("12345").authorities("read").build();
+//		userDetailsService.createUser(user);
+//		userDetailsService.createUser(user1);
+//		auth.userDetailsService(userDetailsService);
+//	}
+//	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
+//// ################################################################
+
+// ################################################################
+//	Using MySql
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		// the dataSource is created by Spring using the database info in application.properties
+		return new JdbcUserDetailsManager(dataSource);
 	}
 	
 	@Bean
