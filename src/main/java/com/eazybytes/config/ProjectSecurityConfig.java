76f469001622a55c10f156e264e855395ef7c2f2
2peaks <woosang@hotmail.com>
2022-01-27 19:52:40 -0800

    Update all the controllers and configure CORS. Re-create and database tables.

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.eazybytes.config;
 
+import java.util.Collections;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,11 +32,24 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 //		http.formLogin();
 //		http.httpBasic();
 		
-		http.authorizeRequests()
+		http.cors().configurationSource(new CorsConfigurationSource() {
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+				config.setAllowedMethods(Collections.singletonList(("*")));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}			
+		}).and()
+		.authorizeRequests()
 		.antMatchers("/myAccount").authenticated()
 		.antMatchers("/myBalance").authenticated()
 		.antMatchers("/myLoans").authenticated()
 		.antMatchers("/myCards").authenticated()
+		.antMatchers("/user").authenticated()
 		.antMatchers("/notices").permitAll()
 		.antMatchers("/contact").permitAll();
 		http.formLogin();
--- src/main/java/com/eazybytes/controller/CardsController.java
+++ src/main/java/com/eazybytes/controller/CardsController.java
@@ -1,13 +1,29 @@
 package com.eazybytes.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.model.Cards;
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CardsRepository;
 
 @RestController
 public class CardsController {
+	
+	@Autowired
+	private CardsRepository cardsRepository;
 
-	@GetMapping("/myCards")
-	public String getCardDetails(String input) {
-		return "Here are the card details from the DB";
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List <Cards> cards = cardsRepository.findByCustomerId(customer.getId());
+		if(cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
 	}
 }
